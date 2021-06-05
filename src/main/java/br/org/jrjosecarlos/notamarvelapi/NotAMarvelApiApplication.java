784992cb14a79a44790000276d264e3bd7916025
José Carlos da Conceição Junior <jr_josecarlos@yahoo.com.br>
2021-06-05 12:12:36 -0300

    setup/configuracao-inicial: adicionado código inicial do projeto

---
+++ src/main/java/br/org/jrjosecarlos/notamarvelapi/NotAMarvelApiApplication.java
@@ -0,0 +1,13 @@
+package br.org.jrjosecarlos.notamarvelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotAMarvelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotAMarvelApiApplication.class, args);
+	}
+
+}
