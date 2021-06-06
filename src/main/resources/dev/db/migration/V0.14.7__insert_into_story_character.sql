82809198cc99b5b6d89ca58a2e3d47952447cd75
José Carlos da Conceição Junior <jr_josecarlos@yahoo.com.br>
2021-06-06 19:44:52 -0300

    feature/criacao-modelos: adicionada migração dev de carga da tabela associativa entre Story e Character

---
+++ src/main/resources/dev/db/migration/V0.14.7__insert_into_story_character.sql
@@ -0,0 +1,8 @@
+INSERT INTO nama.story_character (story_id, character_id) VALUES
+('a55e879d-fb24-424c-bff0-3d1f2657aa55', 'f100b1df-07c7-4f2d-a604-c322c36933d6'),
+('a55e879d-fb24-424c-bff0-3d1f2657aa55', 'a9727005-1b1d-4325-aba8-7954f4151426'),
+('cd2aa155-b116-4705-83c3-4899caa0062c', 'f100b1df-07c7-4f2d-a604-c322c36933d6'),
+('d3e46151-13fc-422d-a8e9-dce7b0631c68', '5f442d4b-f0eb-4e49-aaeb-5526f8dd7ced'),
+('6f88c1ff-07e2-45fe-ac9d-00744dc2d1d1', '6761af20-2ee6-421c-8923-379e45200d8e'),
+('6f88c1ff-07e2-45fe-ac9d-00744dc2d1d1', '5f442d4b-f0eb-4e49-aaeb-5526f8dd7ced'),
+('641c0288-6aee-4d35-bffb-34f9f3b9686a', '740bc432-8a58-4daf-9194-c98da0df6781');
